71be2d9a2732c1f4dbce09b3b104e23323143a4c
Max Novich <mnovich@squareup.com>
2024-09-20 15:49:03 -0700

    added .goosehints support

--- src/main/kotlin/com/block/gooseintellij/actions/GoosePluginStartupActivity.kt
+++ src/main/kotlin/com/block/gooseintellij/actions/GoosePluginStartupActivity.kt
@@ -10,12 +10,23 @@ import com.intellij.openapi.startup.ProjectActivity
 import com.intellij.openapi.wm.ToolWindowManager
 import com.intellij.ide.util.PropertiesComponent
 import com.block.gooseintellij.utils.GooseUtils
+import java.io.File
 
 class GoosePluginStartupActivity : ProjectActivity {
     override suspend fun execute(project: Project) {
         // Check for Goose and SQ Goose availability and paths
         GooseUtils.checkGooseAvailability()
-        
+
+        // Check for .goosehints file and create it if it does not exist
+        val gooseHintsFile = File(project.basePath, ".goosehints")
+        if (!gooseHintsFile.exists()) {
+            gooseHintsFile.createNewFile()
+            val gitignoreFile = File(project.basePath, ".gitignore")
+            gitignoreFile.appendText(System.lineSeparator() + ".goosehints")
+            gitignoreFile.appendText(System.lineSeparator() + ".goose")
+        }
+
+
         ApplicationManager.getApplication().invokeLater {
             val toolWindow = ToolWindowManager.getInstance(project).getToolWindow("Goose Terminal")
             val contentManager = toolWindow?.contentManager
@@ -35,7 +46,7 @@ class GoosePluginStartupActivity : ProjectActivity {
         val contentManager = toolWindow?.contentManager
         val content = contentManager?.getContent(0)
         var gooseTerminal = content?.component as? GooseTerminalWidget
-        
+
         if (gooseTerminal != null) {
             val propertiesComponent = PropertiesComponent.getInstance(project)
             propertiesComponent.setValue("goose.saved.session", project.name)
@@ -43,12 +54,12 @@ class GoosePluginStartupActivity : ProjectActivity {
             gooseTerminal.writeToTerminal(project.name)
             contentManager?.removeContent(content!!, true)
         }
-        
+
         GooseTerminalWidgetFactory().createToolWindowContent(project, toolWindow!!)
         gooseTerminal = contentManager?.getContent(0)?.component as? GooseTerminalWidget
         gooseTerminal?.writeToTerminal("Restarting session...")
         GooseUtils.startGooseSession(true, gooseTerminal, project)
-        
+
         if (!toolWindow.isVisible) {
             toolWindow.activate(null)
             toolWindow.show()
---
+++ src/main/kotlin/com/block/gooseintellij/actions/OpenGooseHintsFileAction.kt
@@ -0,0 +1,24 @@
+package com.block.gooseintellij.actions
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.LocalFileSystem
+import com.intellij.openapi.vfs.VirtualFile
+import java.io.File
+
+class OpenGooseHintsFileAction : AnAction("Open Goose Hints File") {
+    override fun actionPerformed(event: AnActionEvent) {
+        val project = event.project ?: return
+        val filePath = project.basePath + "/.goosehints"
+        val file = File(filePath)
+
+        val virtualFile: VirtualFile? = LocalFileSystem.getInstance().refreshAndFindFileByIoFile(file)
+
+        // Open in editor
+        if (virtualFile != null) {
+            FileEditorManager.getInstance(project).openFile(virtualFile, true)
+        }
+    }
+}
