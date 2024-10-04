ff0058f77202ac29534872a10c410f78fcccefbf
Max Novich <mnovich@squareup.com>
2024-10-04 15:38:27 -0700

    added restart shell action

--- src/main/kotlin/com/block/gooseintellij/toolWindow/GooseTerminalWidgetFactory.kt
+++ src/main/kotlin/com/block/gooseintellij/toolWindow/GooseTerminalWidgetFactory.kt
@@ -1,5 +1,8 @@
 package com.block.gooseintellij.toolWindow
 
+import com.block.gooseintellij.actions.GoosePluginStartupActivity
+import com.intellij.icons.AllIcons
+import com.intellij.icons.AllIcons.Icons
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
@@ -9,10 +12,12 @@ import java.awt.Dimension
 import java.awt.event.ComponentAdapter
 import java.awt.event.ComponentEvent
 import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.wm.impl.content.ToolWindowContentUi
 
 class GooseTerminalWidgetFactory : ToolWindowFactory {
   override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
     val panel = GooseTerminalWidget(toolWindow)
+    toolWindow.getComponent().putClientProperty(ToolWindowContentUi.HIDE_ID_LABEL, "true");
 
     // Compute preferred initial size (30% width of the editor window)
     val fileEditorManager = FileEditorManager.getInstance(project)
@@ -20,6 +25,15 @@ class GooseTerminalWidgetFactory : ToolWindowFactory {
     val editorWindowWidth = editorWindow?.width ?: 0
     val desiredWidth = (editorWindowWidth * 0.3).toInt()
     panel.preferredSize = Dimension(desiredWidth, editorWindow?.height ?: 600)
+
+    // Add Action with Gear Icon
+    val restartAction = object : com.intellij.openapi.actionSystem.AnAction("Restart Shell", "Restart the Shell", AllIcons.Actions.Restart) {
+      override fun actionPerformed(e: com.intellij.openapi.actionSystem.AnActionEvent) {
+        GoosePluginStartupActivity().reInitializeGooseTerminal(project)
+      }
+    }
+
+    toolWindow.setTitleActions(listOf(restartAction))
 
     // Listen for resize events on the terminal panel
     panel.addComponentListener(object : ComponentAdapter() {
@@ -33,7 +47,7 @@ class GooseTerminalWidgetFactory : ToolWindowFactory {
     })
 
     val contentFactory = ApplicationManager.getApplication().getService(ContentFactory::class.java)
-    val content = contentFactory.createContent(panel, "goose-shell", false)
+    val content = contentFactory.createContent(panel, "Goose Chat", false)
     toolWindow.contentManager.addContent(content)
   }
 }
